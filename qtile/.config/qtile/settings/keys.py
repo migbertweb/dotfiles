65853fecfa4384047c7337e5e52d5c62aafa58de
migbertweb <migbertyanez@disroot.org>
2022-06-17 20:14:23 -0400

    Actualizado el shortcut de qtile para abrir Thunar en vez de Nautilus / agregado MPD, MpDris2 y Ncmpcpp

--- qtile/.config/qtile/settings/keys.py
+++ qtile/.config/qtile/settings/keys.py
@@ -59,7 +59,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod], "b", lazy.spawn("firefox")),
 
     # File Explorer
-    ([mod], "e", lazy.spawn("nautilus")),
+    ([mod], "e", lazy.spawn("thunar")),
 
     # Terminal
     ([mod], "Return", lazy.spawn("kitty")),
