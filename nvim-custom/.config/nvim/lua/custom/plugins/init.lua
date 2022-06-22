5d9dbe2f307a90d03322d9bbc4380ef68d39dfd2
migbertweb <migbertyanez@disroot.org>
2022-06-21 23:27:58 -0400

    añadido github copilot

--- nvim-custom/.config/nvim/lua/custom/plugins/init.lua
+++ nvim-custom/.config/nvim/lua/custom/plugins/init.lua
@@ -1,16 +1,20 @@
 -- custom/plugins/init.lua
 
 return {
-   -- Alpha Dashboard
-   ["goolord/alpha-nvim"] = {
-      disable = false,
-   },
-   -- Formatter
-   ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require("custom.plugins.null-ls").setup()
-      end,
-   },
-   -- load it after nvim-lspconfig cuz we lazy loaded lspconfig
+	-- Alpha Dashboard
+	["goolord/alpha-nvim"] = {
+		disable = false,
+	},
+	-- Formatter
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls").setup()
+		end,
+	},
+	-- load it after nvim-lspconfig cuz we lazy loaded lspconfig
+	-- Github Copilot
+	["github/copilot.vim"] = {
+		after = "nvim-lspconfig",
+	},
 }
