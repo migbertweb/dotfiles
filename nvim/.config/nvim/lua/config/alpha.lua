795d68877136732dc5ac2c023744236ecb9f8438
migbertweb <migbertyanez@disroot.org>
2022-10-16 23:15:53 -0400

    iniciando un Nvim-config propio-- volvi a nvchad

--- nvim/.config/nvim/lua/config/alpha.lua
+++ nvim/.config/nvim/lua/config/alpha.lua
@@ -9,11 +9,23 @@ function M.setup()
   local dashboard = require "alpha.themes.dashboard"
   local function header()
     return {
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+      [[                                           bbbbbbbb            ]],
+      [[                                           b::::::b            ]],
+      [[                                           b::::::b            ]],
+      [[                                           b::::::b            ]],
+      [[                                            b:::::b            ]],
+      [[nnnn  nnnnnnnn    vvvvvvv           vvvvvvv b:::::bbbbbbbbb    ]],
+      [[n:::nn::::::::nn   v:::::v         v:::::v  b::::::::::::::bb  ]],
+      [[n::::::::::::::nn   v:::::v       v:::::v   b::::::::::::::::b ]],
+      [[nn:::::::::::::::n   v:::::v     v:::::v    b:::::bbbbb:::::::b]],
+      [[  n:::::nnnn:::::n    v:::::v   v:::::v     b:::::b    b::::::b]],
+      [[  n::::n    n::::n     v:::::v v:::::v      b:::::b     b:::::b]],
+      [[  n::::n    n::::n      v:::::v:::::v       b:::::b     b:::::b]],
+      [[  n::::n    n::::n       v:::::::::v        b:::::b     b:::::b]],
+      [[  n::::n    n::::n        v:::::::v         b:::::bbbbbb::::::b]],
+      [[  n::::n    n::::n         v:::::v          b::::::::::::::::b ]],
+      [[  n::::n    n::::n          v:::v           b:::::::::::::::b  ]],
+      [[  nnnnnn    nnnnnn           vvv            bbbbbbbbbbbbbbbb   ]],
     }
   end
 
@@ -28,8 +40,19 @@ function M.setup()
   local function footer()
     -- Number of plugins
     local total_plugins = #vim.tbl_keys(packer_plugins)
-    local datetime = os.date "%d-%m-%Y  %H:%M:%S"
-    local plugins_text = "\t" .. total_plugins .. " plugins  " .. datetime
+    local datetime = os.date "%d-%m-%Y %H:%M:%S"
+    local plugins_text = 
+      "   "
+      .. total_plugins
+      .. " plugins"
+      .. "   v"
+      .. vim.version().major
+      .. "."
+      .. vim.version().minor
+      .. "."
+      .. vim.version().patch
+      .. "   "
+      .. datetime
 
     -- Quote
     local fortune = require "alpha.fortune"
--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,38 +1,181 @@
 local M = {}
 
+-- Custom actions
+local transform_mod = require("telescope.actions.mt").transform_mod
+local nvb_actions = transform_mod {
+  file_path = function(prompt_bufnr)
+    -- Get selected entry and the file full path
+    local content = require("telescope.actions.state").get_selected_entry()
+    local full_path = content.cwd .. require("plenary.path").path.sep .. content.value
+
+    -- Yank the path to unnamed and clipboard registers
+    vim.fn.setreg('"', full_path)
+    vim.fn.setreg("+", full_path)
+
+    -- Close the popup
+    require("utils").info "File path is yanked "
+    require("telescope.actions").close(prompt_bufnr)
+  end,
+
+  -- VisiData
+  visidata = function(prompt_bufnr)
+    -- Get the full path
+    local content = require("telescope.actions.state").get_selected_entry()
+    local full_path = content.cwd .. require("plenary.path").path.sep .. content.value
+
+    -- Close the Telescope window
+    require("telescope.actions").close(prompt_bufnr)
+
+    -- Open the file with VisiData
+    local term = require "utils.term"
+    term.open_term("vd " .. full_path, { direction = "float" })
+  end,
+}
+
+-- trouble.nvim
+local trouble = require "trouble.providers.telescope"
+local icons = require "config.icons"
+
 function M.setup()
-require('telescope').setup{
-  defaults = {
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
-      }
-    }
-  },
-  pickers = {
-  },
-  extensions = {
-		file_browser = {
-      -- theme = "cursor",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
+  local actions = require "telescope.actions"
+  local telescope = require "telescope"
+
+  -- Custom previewer
+  local previewers = require "telescope.previewers"
+  local Job = require "plenary.job"
+  local preview_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job
+      :new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+          local mime_type = vim.split(j:result()[1], "/")[1]
+
+          if mime_type == "text" then
+            -- Check file size
+            vim.loop.fs_stat(filepath, function(_, stat)
+              if not stat then
+                return
+              end
+              if stat.size > 500000 then
+                return
+              else
+                previewers.buffer_previewer_maker(filepath, bufnr, opts)
+              end
+            end)
+          else
+            vim.schedule(function()
+              vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY FILE" })
+            end)
+          end
+        end,
+      })
+      :sync()
+  end
+
+  telescope.setup {
+    defaults = {
+      prompt_prefix = icons.ui.Telescope .. " ",
+      selection_caret = " ",
+      -- path_display = { "smart" },
+      buffer_previewer_maker = preview_maker,
       mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-n>"] = actions.cycle_history_next,
+          ["<C-p>"] = actions.cycle_history_prev,
+          ["<c-z>"] = trouble.open_with_trouble,
         },
-        ["n"] = {
-          -- your custom normal mode mappings
+      },
+      history = {
+        path = vim.fn.stdpath "data" .. "/telescope_history.sqlite3",
+        limit = 100,
+      },
+      border = {},
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      color_devicons = true,
+    },
+    pickers = {
+      find_files = {
+        theme = "ivy",
+        mappings = {
+          n = {
+            ["y"] = nvb_actions.file_path,
+            ["s"] = nvb_actions.visidata,
+          },
+          i = {
+            ["<C-y>"] = nvb_actions.file_path,
+            ["<C-s>"] = nvb_actions.visidata,
+          },
+        },
+        hidden = true,
+        find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+      },
+      git_files = {
+        theme = "dropdown",
+        mappings = {
+          n = {
+            ["y"] = nvb_actions.file_path,
+            ["s"] = nvb_actions.visidata,
+          },
+          i = {
+            ["<C-y>"] = nvb_actions.file_path,
+            ["<C-s>"] = nvb_actions.visidata,
+          },
         },
       },
     },
+    extensions = {
+      arecibo = {
+        ["selected_engine"] = "google",
+        ["url_open_command"] = "xdg-open",
+        ["show_http_headers"] = false,
+        ["show_domain_icons"] = false,
+      },
+      media_files = {
+        filetypes = { "png", "webp", "jpg", "jpeg", "pdf", "mp4", "webm" },
+        find_cmd = "fd",
+      },
+      bookmarks = {
+        selected_browser = "brave",
+        url_open_command = nil,
+        url_open_plugin = "open_browser",
+        full_path = true,
+        firefox_profile_name = nil,
+      },
+      project = {
+        hidden_files = false,
+        theme = "dropdown",
+      },
+      -- aerial = {
+      --   show_nesting = true,
+      -- },
+    },
   }
-}
--- you need to call load_extension, somewhere after setup function:
-require'telescope'.load_extension('project')
-require("telescope").load_extension "file_browser"
+
+  require("neoclip").setup() -- https://github.com/AckslD/nvim-neoclip.lua/issues/5
+
+  telescope.load_extension "fzf"
+  telescope.load_extension "project" -- telescope-project.nvim
+  telescope.load_extension "repo"
+  telescope.load_extension "file_browser"
+  telescope.load_extension "projects" -- project.nvim
+  telescope.load_extension "dap"
+  telescope.load_extension "frecency"
+  telescope.load_extension "neoclip"
+  telescope.load_extension "smart_history"
+  telescope.load_extension "arecibo"
+  telescope.load_extension "media_files"
+  telescope.load_extension "bookmarks"
+  telescope.load_extension "aerial"
+  telescope.load_extension "gh"
+  telescope.load_extension "zoxide"
+  telescope.load_extension "cder"
+  telescope.load_extension "harpoon"
+  -- telescope.load_extension "ui-select"
+  -- telescope.load_extension "flutter" -- Flutter
 end
 
- return M
+return M
