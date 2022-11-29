5b94271901c5794063348ef4ffcacd282d071a64
migbertweb <migbertyanez@disroot.org>
2022-11-28 21:44:07 -0400

    mejoras de codigo simples en ui.lua

--- nvim-custom/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim-custom/.config/nvim/lua/custom/plugins/ui.lua
@@ -5,16 +5,13 @@ local fn = vim.fn
 --	or require("nvchad_ui.icons").statusline_separators[sep_style]
 -- local sep_l = separators["left"]
 -- local sep_r = separators["right"]
-
 return {
 	cursor_position = function()
 		--		local left_sep = "%#St_pos_sep#" .. sep_l
 		local current_line = fn.line(".")
-		--		local total_line = fn.line("$")
+		local total_line = fn.line("$")
 		--		local text = math.modf((current_line / total_line) * 100) .. tostring("%%")
-		--		local total_line = fn.line("$")
 		local pos = fn.col(".")
-		--return st_modules.cursor_position() .. " lin: " .. current_line .. " col " .. pos
-		return st_modules.cursor_position() .. "lin:" .. current_line .. " col:" .. pos
+		return st_modules.cursor_position() .. "lin:" .. current_line .. "/" .. total_line .. " col:" .. pos
 	end,
 }
