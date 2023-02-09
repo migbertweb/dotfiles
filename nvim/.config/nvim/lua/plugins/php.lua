4f497dabb86c95f4f7997a8882c2ee219f830845
migbertweb <migbertyanez@disroot.org>
2023-02-08 23:12:16 -0300

    instalando Lazyvim- añadiendo plugins y otras cosas

---
+++ nvim/.config/nvim/lua/plugins/php.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "php" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+        opts = {
+      -- make sure mason installs the server
+      servers = {
+        intelephense = {
+          },
+      },
+    },
+  },
+}
