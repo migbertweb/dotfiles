fb0424f039fd752d976b582f3901a576e733a64c
migbertweb <migbertyanez@disroot.org>
2023-06-21 13:52:14 -0300

    añadiendo deno-fmt
    añadiendo deno-fmt para formatear js, jsx, ts, tsx
    especificando utilizar comillas simples y sin punto y coma
    - prettier solo para html y css

--- nvim/.config/nvim/lua/plugins/extras/formatting/init.lua
+++ nvim/.config/nvim/lua/plugins/extras/formatting/init.lua
@@ -3,15 +3,16 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.deno_fmt.with({ extra_args = {"--single-quote", "--no-semicolons"}}))
-      table.insert(opts.sources, nls.builtins.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }))
+      table.insert(opts.sources,
+        nls.builtins.formatting.deno_fmt.with({ extra_args = { "--single-quote", "--no-semicolons" } }))
+      table.insert(opts.sources, nls.builtins.formatting.prettier.with({ filetypes = { "html", "css" } }))
     end,
   },
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "deno")
-            table.insert(opts.ensure_installed, "prettier")
+      table.insert(opts.ensure_installed, "prettier")
     end,
   },
 
