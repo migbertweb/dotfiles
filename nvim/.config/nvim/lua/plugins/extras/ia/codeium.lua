ef539f7142e69ffa13965bf9f41857c7a43f244a
migbertweb <migbertyanez@disroot.org>
2023-06-26 16:14:21 -0300

    delete build auth

--- nvim/.config/nvim/lua/plugins/extras/ia/codeium.lua
+++ nvim/.config/nvim/lua/plugins/extras/ia/codeium.lua
@@ -2,7 +2,7 @@ return {
   {
     "Exafunction/codeium.vim",
     event = "InsertEnter",
-    build = ":Codeium Auth",
+    -- build = ":Codeium Auth",
     -- stylua: ignore
     config = function ()
       vim.g.codeium_disable_bindings = 1
