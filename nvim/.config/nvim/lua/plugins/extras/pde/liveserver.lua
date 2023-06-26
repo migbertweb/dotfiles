13e1a305328ff31607dd416396b57c61595694a1
migbertweb <migbertyanez@disroot.org>
2023-06-26 16:14:02 -0300

    config liveserver only web filetypes

--- nvim/.config/nvim/lua/plugins/extras/pde/liveserver.lua
+++ nvim/.config/nvim/lua/plugins/extras/pde/liveserver.lua
@@ -4,8 +4,7 @@ return {
     "barrett-ruth/live-server.nvim",
     -- event = "VeryLazy",
     build = "yarn global add live-server",
-    config = true,
-    keys = {
+--[[    keys = {
       {
         "<leader>cs",
         function()
@@ -20,6 +19,15 @@ return {
         end,
         desc = "󱞐 LiveSrver Stop",
       },
-    },
+    },--]]
+    config = function ()
+      vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    callback = function ()
+        vim.keymap.set("n", "<leader>cs", function() liveserver.liveserver("start") end, { desc = "󱞒 LiveServer Start"})     
+        vim.keymap.set("n", "<leader>cx", function() liveserver.liveserver("stop") end, { desc = "󱞐 LiveSrver Stop" })
+    end
+})
+    end
   },
 }
