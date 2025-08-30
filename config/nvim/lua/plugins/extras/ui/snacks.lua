ef8d3fba676323ac07dbfd82c00f5d3e4f0096e3
migbertweb <migbertyanez@disroot.org>
2025-08-30 09:22:04 -0300

    mejoras a nvim

--- config/nvim/lua/plugins/extras/ui/snacks.lua
+++ config/nvim/lua/plugins/extras/ui/snacks.lua
@@ -2,27 +2,27 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
-      width = 50,
-      row = nil, -- dashboard position. nil for center
-      col = nil, -- dashboard position. nil for center
-      pane_gap = 4, -- empty columns between vertical panes
+      width = 70,
+      row = nil,    -- dashboard position. nil for center
+      col = nil,    -- dashboard position. nil for center
+      pane_gap = 6, -- empty columns between vertical panes
       preset = {
-        header = [[
-███╗   ███╗██╗ ██████╗ ██████╗ ███████╗██████╗ ████████╗
-████╗ ████║██║██╔════╝ ██╔══██╗██╔════╝██╔══██╗╚══██╔══╝
-██╔████╔██║██║██║  ███╗██████╔╝█████╗  ██████╔╝   ██║   
-██║╚██╔╝██║██║██║   ██║██╔══██╗██╔══╝  ██╔══██╗   ██║   
-██║ ╚═╝ ██║██║╚██████╔╝██████╔╝███████╗██║  ██║   ██║   
-╚═╝     ╚═╝╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   
-]],
-
         -- stylua: ignore
+        header = [[
+   ███╗   ███╗██╗ ██████╗ ██████╗ ███████╗██████╗ ████████╗
+   ████╗ ████║██║██╔════╝ ██╔══██╗██╔════╝██╔══██╗╚══██╔══╝
+██╔████╔██║██║██║  ███╗██████╔╝█████╗  ██████╔╝   ██║
+██║╚██╔╝██║██║██║   ██║██╔══██╗██╔══╝  ██╔══██╗   ██║
+██║ ╚═╝ ██║██║╚██████╔╝██████╔╝███████╗██║  ██║   ██║
+╚═╝     ╚═╝╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝
+
+]],
         ---@type snacks.dashboard.Item[]
         keys = {
           { icon = " ", key = "f", desc = "Buscar", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "n", desc = "Nuevo Archivo", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Buscar Texto", action = ":lua Snacks.dashboard.pick('live_grep')" },
-      --    { icon = " ", key = "r", desc = "Recientes", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          --    { icon = " ", key = "r", desc = "Recientes", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restaurar Sesion", section = "session" },
           { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
@@ -46,12 +46,12 @@ return {
         {
           pane = 2,
           section = "terminal",
-          cmd = "fastfetch --config minimal",
-          height = 8,
-          indent = 14,
+          cmd = "fastfetch --config os",
+          height = 10,
+          indent = 16,
           padding = 1,
         },
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "keys",  gap = 1, padding = 1 },
         {
           pane = 2,
           icon = " ",
