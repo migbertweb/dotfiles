1a27179834e078d8cf837a34ba1908ad2d786b8f
migbertweb <migbertyanez@disroot.org>
2025-09-06 22:38:41 -0300

    grandes cambios en hyprland

--- config/hypr/scripts/powermenu.sh
+++ config/hypr/scripts/powermenu.sh
@@ -1,20 +1,2 @@
-#!/usr/bin/env bash
-
-options="  Bloquear sesión
-󰤄  Suspender
-  Reiniciar
-  Apagar
-󰍃  Cerrar sesión"
-
-# ruta a tu estilo personalizado
-STYLE="$HOME/.config/hypr/wofi/powermenu.css"
-
-chosen=$(echo -e "$options" | wofi --dmenu -W 280 -H 30 -j -w 1 -s "$STYLE")
-
-case "$chosen" in
-    "  Bloquear sesión") loginctl lock-session ;;
-    "󰤄  Suspender") systemctl suspend ;;
-    "  Reiniciar") systemctl reboot ;;
-    "  Apagar") systemctl poweroff ;;
-    "󰍃  Cerrar sesión") hyprctl dispatch exit ;;
-esac
+#!/bin/bash
+nwg-bar -t ~/.config/hypr/nwg-bar/bar.json -s ~/.config/hypr/nwg-bar/style.css    
