a5c3cd886d86bc2332efe986d7fcb10c673a3097
migbertweb <migbertyanez@disroot.org>
2026-03-29 21:49:55 -0300

    salvaguarda de la password

--- config/hypr/scripts/unlock_keyring.sh
+++ config/hypr/scripts/unlock_keyring.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Reemplaza TU_CONTRASEÑA_AQUÍ con tu contraseña real. 
-# ADVERTENCIA: Esto es inseguro, tu contraseña será visible en este archivo.
-PASSWORD="gpiro2178"
+# Lee la contraseña desde el archivo .pass en el home
+PASSWORD=$(cat ~/.pass)
 
 # Desbloquear el llavero y exportar las variables de entorno necesarias
 eval $(echo -n "$PASSWORD" | gnome-keyring-daemon --replace --daemonize --login --components=secrets,pkcs11,ssh)
